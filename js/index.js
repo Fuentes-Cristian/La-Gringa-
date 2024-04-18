41b1d16fc8c1b42729ea21c74dca02ca00d02117
Fuentes Cristian <110264073+Fuentes-Cristian@users.noreply.github.com>
2024-04-17 22:08:25 -0300

    nuevas actualizaciones

--- js/index.js
+++ js/index.js
@@ -1,5 +1,34 @@
-const form = document.querySelector("form");
+const form = document.getElementById("form");
+const datos = []; // Array para almacenar los datos del formulario
 
-form.addEventListener("submit", ()=>{
-    //console.log(form);
-})
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    //valores tomados por inputs que ingresa el usuario
+    const nombre = document.getElementById("nombre").value;
+    const clave = document.getElementById("clave").value;
+
+
+    // Crear un objeto para almacenar los datos del formulario
+    const formData = {
+        nombre: nombre,
+        clave: clave
+    };
+
+    usuario()
+
+    //guardar los datos ingresados en localStorage
+    function usuario() {
+        
+        // Agregar los datos del formulario al array
+        datos.push(formData);        
+        
+        const baseDatos = JSON.stringify(datos);
+        localStorage.setItem("Datos del usuario", baseDatos)
+
+    }
+
+    form.reset()
+});
+
+
